826d3893c0c75d14313d8210cfb3f2a4f9c8d681
Miguel <sastre113@gmail.com>
2020-11-25 21:46:34 +0100

    fix: archivo axios

--- public/js/axios.js
+++ public/js/axios.js
@@ -1,16 +1,9 @@
-const { default: Axios } = require("axios");
-
-console.log("hola axios");
-
 const postRequest = document.querySelector('#post');
 const readAllRequest = document.querySelector('#readAll');
 const readRequest = document.querySelector('#read');
 const updateRequest = document.querySelector('#update');
 const deleteRequest = document.querySelector('#delete');
 
-const testPrueba = {
-    // Hay que poner los datos del esquema de DB
-}
 
 postRequest.addEventListener('click', async (e) => {
     e.preventDefault();
