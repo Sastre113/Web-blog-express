3514899abd9472b59fdea613817c86952a0c0289
Miguel <sastre113@gmail.com>
2020-11-26 00:20:14 +0100

    fix: archivo axios

--- public/js/axios.js
+++ public/js/axios.js
@@ -6,10 +6,19 @@ const deleteRequest = document.querySelector('#delete');
 
 
 postRequest.addEventListener('click', async (e) => {
-    e.preventDefault();
-    const res = await axios.post('https://web--express-mas-app.herokuapp.com/api/s',
-    {data: {}});
-    console.log(res);
+    console.log(e.target)
+    e.preventDefault()
+   
+    console.log(e.target.elements)
+    
+    const resultado = await axios.post('https://x-express-web.herokuapp.com/api/tests', {
+        question: e.target.elements.question.value,
+        answer1: e.target.elements.answer1.value,
+        answer2: e.target.elements.answer2.value,
+        answer3: e.target.elements.answer3.value,
+        answer4: e.target.elements.answer4.value
+    })
+    console.log(resultado)
 });
 
 readAllRequest.addEventListener('click',async (e) => {
