715afe8948f12f85caaca4f86203ff7d0d2ca170
Miguel <sastre113@gmail.com>
2020-11-26 10:28:18 +0100

    fix:

--- public/js/axios.js
+++ public/js/axios.js
@@ -26,7 +26,7 @@ readAllRequest.addEventListener('click',async (e) => {
     e.preventDefault(); 
     const res = await axios.get('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
     
-    document.querySelector('#HUD').innerHTML = res.innerHTML;
+    document.querySelector('#HUD').innerHTML = res.elements.innerHTML;
     console.log(res);
 });
 
