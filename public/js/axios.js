7ebce465835b5befce1de2da124640b3673c748a
Miguel <sastre113@gmail.com>
2020-11-27 15:49:46 +0100

    Añadido formulario ocultable

--- public/js/axios.js
+++ public/js/axios.js
@@ -4,21 +4,33 @@ const readRequest = document.querySelector('#read');
 const updateRequest = document.querySelector('#update');
 const deleteRequest = document.querySelector('#delete');
 
-const contenido = document.querySelector('#right');
-const form = document.querySelector('#formDin');
+const contenido = document.querySelector('.right');
+const form = document.querySelector('#formularioDin');
+
+const contenidos = ["#HUD","#formularioDin","#getDatos","#getUnDato","#updateDatos","#deleteDatos"]
+
+// Funcion auxiliar
+
+function mostrarSelector(target){
+    contenidos.forEach( campo =>{
+        if(target != campo)
+            document.querySelector(campo).style.display = "none";
+        else
+            document.querySelector(target).style.display = "block";
+    });
+}
+
+// Boton Post
 
 postRequest.addEventListener('mouseover',  (e) =>{
-    e.preventDefault()
-    console.log("has pasado por encima del boton")
-    document.querySelector('#HUD').innerHTML="";
-    document.getElementById('formDin').style.display = "inline";
+    e.preventDefault();
+    mostrarSelector("#formularioDin");
 });
 
 form.addEventListener('submit', async (e) => {
-    console.log(e.target)
-    e.preventDefault()
-   
-    console.log()
+    console.log(e.target);
+    e.preventDefault();
+
     let title = document.querySelector('#titulo')
     let snippet = document.querySelector('#snippet')
     let body = document.querySelector('#body')
@@ -34,25 +46,56 @@ form.addEventListener('submit', async (e) => {
     console.log(resultado)
 });
 
-// readAllRequest.addEventListener('click',async (e) => {
-//     e.preventDefault(); 
-//     document.querySelector('#HUD').innerHTML = "<h1> HOLA PEPE</h1>";
-//     const res = await axios.get('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
+// Boton Obtener todos los datos
+
+
+readAllRequest.addEventListener('mouseover',  (e) =>{
+    e.preventDefault()
+    mostrarSelector("#getDatos");
+});
+
+readAllRequest.addEventListener('click',async (e) => {
+    e.preventDefault(); 
+    document.querySelector('#HUD').innerHTML = "";
+    const res = await axios.get('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
     
-//     console.log(res);
-// });
+    console.log(res);
+});
 
-// readRequest.addEventListener('click',async (e) => {
-//     e.preventDefault();
-//     const res = await axios.get('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
-// });
+// Boton Obtener 1 dato
 
-// updateRequest.addEventListener('click',async (e) => {
-//     e.preventDefault();
-//     const res = await axios.patch('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
-// });
 
-// deleteRequest.addEventListener('click',async (e) => {
-//     e.preventDefault();
-//     const res = await axios.delete('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
-// });
+readRequest.addEventListener('mouseover',  (e) =>{
+    e.preventDefault()
+    mostrarSelector("#getUnDato");
+});
+
+readRequest.addEventListener('click',async (e) => {
+    e.preventDefault();
+    const res = await axios.get('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
+});
+
+
+// Boton Actualizar datos
+
+updateRequest.addEventListener('mouseover',  (e) =>{
+    e.preventDefault()
+    mostrarSelector("#updateDatos");
+});
+
+updateRequest.addEventListener('click',async (e) => {
+    e.preventDefault();
+    const res = await axios.patch('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
+});
+
+
+deleteRequest.addEventListener('mouseover',  (e) =>{
+    e.preventDefault()
+    mostrarSelector("#deleteDatos");
+});
+
+
+deleteRequest.addEventListener('click',async (e) => {
+    e.preventDefault();
+    const res = await axios.delete('https://web-blog-express-mas-app.herokuapp.com/api/blogs',);
+});
